acf97bf3b80e1da1e45c873836898d3c37962fad
3302011 <fatinealj@hotmail.fr>
2018-05-14 16:08:29 +0200

    code boucles ok reste simd

--- Bentires_Zounias/Partie2/q3.2/Code_sequentiel/exec.sh
+++ Bentires_Zounias/Partie2/q3.2/Code_sequentiel/exec.sh
@@ -1,9 +1,11 @@
+clear
+
 mkdir /tmp/3302011
 
 make clean
 
 make
 
-./bin/shalw 
+./bin/shalw --export --export-path /tmp/3302011/
 
 ./visu.py /tmp/3302011/shalw_256x256_T1000.sav
--- Bentires_Zounias/Partie2/q3.2/Code_sequentiel/src/forward.c
+++ Bentires_Zounias/Partie2/q3.2/Code_sequentiel/src/forward.c
@@ -4,32 +4,49 @@
 #include <export.h>
 #include <immintrin.h> 
 
+void forward(void) {
+  FILE *file = NULL;
+  double svdt = 0.;
+  int t = 0;
 
-int execution(int t, int i, int j)
-{
+ __m256d HP, UP, VP, HF, UF, VF; 
+ __m256d hf1,hf2, h_alpha,h_hfil, h_hphy,h2_hphy,h2_uphy,h2_vphy,h2_hfil,h2_ufil,h2_vfil,tmp; 
 
-  __m256d hf1,hf2, h_alpha,h_hfil, h_hphy,h2_hphy, tmp; 
+
+  if (file_export) {
+    file = create_file();
+    export_step(file, t);
+  }
+    
+  for (t = 1; t < nb_steps; t++) {
+    if (t == 1) {
+      svdt = dt;
+      dt = 0;
+    }
+    if (t == 2){
+      dt = svdt / 2.;
+    }
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+    for (int j = 0; j < size_y; j++) {
+      for (int i = 0; i < size_x; i++) {
+
+  // variables 
 
   h_alpha=_mm256_set1_pd(alpha); 
   h_hfil=_mm256_set1_pd(HFIL(t - 1, i, j));
   h_hphy=_mm256_set1_pd(HPHY(t - 1, i, j));
+
   h2_hphy=_mm256_set1_pd(HPHY(t, i, j));
+  h2_uphy=_mm256_set1_pd(UPHY(t, i, j));
+  h2_vphy=_mm256_set1_pd(VPHY(t, i, j));
+  h2_hfil=_mm256_set1_pd(HFIL(t, i, j));
+  h2_ufil=_mm256_set1_pd(UFIL(t, i, j));
+  h2_vfil=_mm256_set1_pd(VFIL(t, i, j));
 
-  if (t <= 2)
-  {
-    HPHY(t, i, j)=HPHY(t, i, j);
-    UPHY(t, i, j)=UPHY(t, i, j); 
-    VPHY(t, i, j)=VPHY(t, i, j); 
-  }
 
-  hf1 = _mm256_mul_pd(h_alpha ,h_hfil);
-  hf2 = hf1 - 2*h_hphy;
-  tmp=_mm256_add_pd(h_hphy,h2_hphy);
-
-  HPHY(t, i, j)=_mm256_add_pd(tmp,hf2);
-  printf("%p",_mm256_add_pd(tmp,hf2));
-  UPHY(t, i, j)=UPHY(t - 1, i, j) + alpha * (UFIL(t - 1, i, j) - 2 * UPHY(t - 1, i, j) + UPHY(t, i, j));
-  VPHY(t, i, j)=VPHY(t - 1, i, j) + alpha * (VFIL(t - 1, i, j) - 2 * VPHY(t - 1, i, j) + VPHY(t, i, j));
+  // Partie 1 : hPhy_forward
 
   double c, d;
   double b, e, f, g;
@@ -45,8 +62,10 @@ int execution(int t, int i, int j)
   HPHY(t,i,j)=HFIL(t - 1, i, j) - dt * hmoy * ((UPHY(t - 1, i, j) - c) / dx +(d - VPHY(t - 1, i, j)) / dy);
 
 
+  //Partie 2 : uPhy_forward
+
   if (i == size_x - 1)
-    return 0.;
+    UPHY(t,i,j)=0.;
 
   b = 0.;
   if (i < size_x - 1)
@@ -63,12 +82,13 @@ int execution(int t, int i, int j)
   g = 0.;
   if (i < size_x - 1 && j < size_y - 1)
     g = VPHY(t - 1, i + 1, j + 1);
-
-  UPHY(t,i,j)=UFIL(t - 1, i, j) +dt * ((-grav / dx) * (b - HPHY(t - 1, i, j)) + (pcor / 4.) * (VPHY(t - 1, i, j) + e + f + g) -(dissip * UFIL(t - 1, i, j)));
-
+ 
+    UPHY(t,i,j)=UFIL(t - 1, i, j) +dt * ((-grav / dx) * (b - HPHY(t - 1, i, j)) + (pcor / 4.) * (VPHY(t - 1, i, j) + e + f + g) -(dissip * UFIL(t - 1, i, j)));
+ 
+  //Partie 3 : vPhy_forward
 
   if (j == 0)
-    return 0.;
+    VPHY(t,i,j)=0.;
 
   c = 0.;
   if (j > 0)
@@ -85,42 +105,61 @@ int execution(int t, int i, int j)
   f = 0.;
   if (j > 0)
     f = UPHY(t - 1, i, j - 1);
+ 
+    VPHY(t,i,j)=VFIL(t - 1, i, j) +dt * ((-grav / dy) * (HPHY(t - 1, i, j) - c) -(pcor / 4.) * (d + e + f + UPHY(t - 1, i, j)) -(dissip * VFIL(t - 1, i, j)));
 
-  VPHY(t,i,j)=VFIL(t - 1, i, j) +dt * ((-grav / dy) * (HPHY(t - 1, i, j) - c) -(pcor / 4.) * (d + e + f + UPHY(t - 1, i, j)) -(dissip * VFIL(t - 1, i, j)));
 
-}
+  //Partie 4 : hFil_forward
 
-void forward(void) {
-  FILE *file = NULL;
-  double svdt = 0.;
-  int t = 0;
-  __m256d HP, UP, VP, HF, UF, VF; 
-  
-  if (file_export) {
-    file = create_file();
-    export_step(file, t);
-  }
-    
-  for (t = 1; t < nb_steps; t++) {
-    if (t == 1) {
-      svdt = dt;
-      dt = 0;
-    }
-    if (t == 2){
-      dt = svdt / 2.;
-    }
+  if (t <= 2)
+    HFIL(t, i, j)=HPHY(t, i, j);
 
-    for (int j = 0; j < size_y; j++) {
-      for (int i = 0; i < size_x; i++) {
+  else 
+    HFIL(t,i,j)=HPHY(t - 1, i, j) +alpha * (HFIL(t - 1, i, j) - 2 * HPHY(t - 1, i, j) + HPHY(t, i, j));//_mm256_add_pd(tmp,hf2);
 
-        execution(t,i,j); 
+  //Partie 5 : uFil_forward 
 
-        HP = _mm256_load_pd(&HPHY(t,i,j));
+  if(t<=2)
+    UFIL(t, i, j)=UPHY(t, i, j);
+
+  else 
+    UFIL(t, i, j)=UPHY(t - 1, i, j) + alpha * (UFIL(t - 1, i, j) - 2 * UPHY(t - 1, i, j) + UPHY(t, i, j));
+
+
+  //Partie 6 : vFil_forward
+
+  if(t<=2)
+    VFIL(t, i, j)=VPHY(t, i, j);
+
+  else 
+    VFIL(t, i, j)=VPHY(t - 1, i, j) + alpha * (VFIL(t - 1, i, j) - 2 * VPHY(t - 1, i, j) + VPHY(t, i, j));
+
+
+    //h2_hphy=h2_hphy; 
+    //h2_hphy=h2_hphy;  
+    //h2_hphy=h2_vphy; 
+
+ // hf1 = _mm256_mul_pd(h_alpha ,h_hfil);
+  //hf2 = hf1 - 2*h_hphy;
+  //tmp=_mm256_add_pd(h_hphy,h2_hphy);
+
+
+
+      /* HP = _mm256_load_pd(&HPHY(t,i,j));
         UP = _mm256_load_pd(&UPHY(t,i,j));
         VP = _mm256_load_pd(&VPHY(t,i,j));
         HF = _mm256_load_pd(&HFIL(t,i,j));
         UF = _mm256_load_pd(&UFIL(t,i,j));
-        VF = _mm256_load_pd(&VFIL(t,i,j));
+        VF = _mm256_load_pd(&VFIL(t,i,j));*/
+
+
+      //pour récupérer les infos de la mémoire 
+       //_mm256_store_pd(&HPHY(t,i,j),HP); 
+        //_mm256_store_pd(&UPHY(t,i,j),UP); 
+        //_mm256_store_pd(&VPHY(t,i,j),VP); 
+        //_mm256_store_pd(&HFIL(t,i,j),HF); 
+        //_mm256_store_pd(&UFIL(t,i,j),UF); 
+        //_mm256_store_pd(&VFIL(t,i,j),VF); 
       }
     }
 
@@ -128,8 +167,6 @@ void forward(void) {
       export_step(file, t);
     }
 
-
-    
     if (t == 2) {
       dt = svdt;
     }
